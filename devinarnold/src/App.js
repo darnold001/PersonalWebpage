0ee6bb4498590d29e281c181d0b67cfb1a0c4267
Devin Arnold <dbarnold321@gmail.com>
2019-09-22 20:43:12 -0600

    added hamburger

--- devinarnold/src/App.js
+++ devinarnold/src/App.js
@@ -1,27 +1,22 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
-import computer from './Components/images/Computer.jpg'
+import Hamburger from './Components/HamburgerButton'
+import computer from './Components/images/CODE.jpg'
 import './App.css';
 
 function App() {
+
+
   return (
 
-    <Router>
+    
     <div className = 'app' style = {{ backgroundImage: "url("+computer+")"}}>
       <div className = 'WelcomeContainer'>
-       <h2 className = 'name'>Devin Arnold</h2>
+       {/* <h2 className = 'name'>Devin Arnold</h2> */}
         <h1 className = 'WelcomeTitle'> Developer / Designer</h1>
-        <Link className = 'link' to = '/'> </Link>
+        <Hamburger className = 'hamburger'></Hamburger>
         </div>
-
-    <Switch>
-      {/* <Route exact path ='/' render={(props)=><Welcome {...props}/>}/> */}
-      {/* <Route path= '/about' render ={(props)=><LandlordPage{...props}/>}/>
-      <Route path= '/portfolio' render ={(props)=><TenantPage{...props}/>}/>
-      <Route component={AppNotFound}/> */}
-    </Switch>
     </div>
-    </Router>
+
   );
 }
 
---
+++ devinarnold/src/Components/about.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Hamburger from '../Components/HamburgerButton'
+export default class About extends Component{
+
+
+    render(){
+        return <div>
+            <Hamburger></Hamburger>
+            <h1> About Me</h1>
+        </div>
+    }
+}
